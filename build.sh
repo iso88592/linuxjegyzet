6c98a8e2525964ab47693f257ce10d1c59d93e41
Tibor Várkonyi <mocmang@gmail.com>
2020-12-17 19:28:49 +0100

    Fix LaTeX warnings

--- build.sh
+++ build.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
+NOBUILD=false
+
+if [ "$1" == "--nobuild" ]; then
+    NOBUILD=true
+fi
+
 if [ -e src/tavjegyzet.pdf ]; then
     rm src/tavjegyzet.pdf
 fi
 
-docker build -t mocman/lyx:1.0 . && docker run -v ${PWD}/src:/data:Z mocman/lyx:1.0 /bin/lyx --export-to pdf tavjegyzet.pdf tavjegyzet.lyx
+if [ "$NOBOUILD" == false ]; then
+    docker build -t mocman/lyx:1.0 .
+fi
+docker run -v ${PWD}/src:/data:Z mocman/lyx:1.0 /bin/lyx --export-to pdf tavjegyzet.pdf tavjegyzet.lyx
 if [ -e src/tavjegyzet.pdf ]; then
     evince src/tavjegyzet.pdf 2>&1 >/dev/null &
 else
